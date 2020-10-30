e27250c5658eb62c943fb2d0eff9be41334258cd
Gareth Tribello <gareth.tribello@gmail.com>
2020-10-30 10:08:09 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def myvariable( probs ) :
+  myrand = np.random.uniform(0,1)
+  myvar, accum = 0, probs[0]
+  while   
+    # You will need to write contents of the while loop here and the 
+    # condition for leaving the loop on the previous line.  Notice that
+    # I have defined three quantities and written a return statement below
+    # to give you a clue as to how to proceed.
+    
+    
+  return myvar 
+  
+probs = np.array([0.5, 0.1, 0.2, 0.05, 0.15 ])
+print( myvariable( probs ), myvariable( probs ), myvariable( probs ) )
