85c9fecb0090a79e71ac6911551fb5621cbd6097
Gareth Tribello <gareth.tribello@gmail.com>
2021-07-15 12:45:40 +0100

    Added answers

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import numpy as np
 def myvariable( probs ) :
   myrand = np.random.uniform(0,1)
   myvar, accum = 0, probs[0]
-  while   
+  while myrand>accum :    
     # You will need to write contents of the while loop here and the 
     # condition for leaving the loop on the previous line.  Notice that
     # I have defined three quantities and written a return statement below
     # to give you a clue as to how to proceed.
-    
+    myvar = myvar + 1
+    accum = accum + probs[myvar] 
     
   return myvar 
   
